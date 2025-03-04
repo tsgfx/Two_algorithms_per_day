12e81d3cb4d58cc12c4da00813fd5cbfacfbb056
tsgfx <grcsxb269@163.com>
2025-03-05 00:56:59 +0800

    day1

---
+++ day1/KY15_abc.py
@@ -0,0 +1,5 @@
+for a in range(10):
+    for b in range(10):
+        for c in range(10):
+            if 100 * a + 110 * b + 12 * c == 532:
+                print(a, b, c)
---
+++ day1/a+b_problem.py
@@ -0,0 +1,3 @@
+# http://bailian.openjudge.cn/practice/1000/
+a, b = map(int, input().split())
+print(a+b)    
